aca26935a450cb58281f09619e24aad17bacd4e9
Aleksander <troskoalexandero@gmail.com>
2025-08-19 23:47:09 +0200

    more additive joins

--- queries.sql
+++ queries.sql
@@ -97,4 +97,111 @@ WHERE carrier='UA' OR carrier='AA';
 SELECT fl.dest, fl.origin, al.name
 FROM flights AS fl
 CROSS JOIN airlines AS al
-WHERE fl.origin IN ('EWR') AND al.carrier IN ('HA');
+WHERE fl.origin IN ('JFK') AND al.carrier IN ('HA');
+
+
+-- SELF JOIN (сравнивать колонку с разными значениями в ней самой, в одной таблице)
+
+-- Сейчас написал сравнение дней в колонке по месяцам из flights
+SELECT fl1.day AS day1, fl2.day AS day2, fl1.month
+FROM flights AS fl1
+INNER JOIN flights AS fl2
+ON fl1.month=fl2.month
+AND fl1.day<>fl2.day
+LIMIT 40
+/* Запрос (условно): 
+Берёт первый рейс 1-го января из таблицы fl1.
+Соединяет его со всеми сотнями рейсов, которые произошли 2-го января в таблице fl2.
+Потом соединяет его со всеми сотнями рейсов 3-го января и т.д.
+Затем он берёт второй рейс 1-го января и повторяет весь процесс.
+*/;
+
+SELECT fl1.flight AS first_flight, fl2.flight AS second_flight, fl1.origin AS first_origin, fl2.origin AS second_origin, fl1.dest, fl2.sched_arr_time AS second_arr_time, fl1.sched_arr_time AS first_arr_time, fl1.sched_arr_time - fl2.sched_arr_time AS difference
+FROM flights AS fl1
+INNER JOIN flights AS fl2
+ON fl1.dest = fl2.dest
+AND fl1.day = fl2.day
+AND fl1.month = fl2.month
+AND fl1.sched_arr_time > fl2.sched_arr_time
+AND (fl1.sched_arr_time - fl2.sched_arr_time) <= 5
+ORDER BY difference ASC
+/* Данный SQL-запрос выполняет поиск пар рейсов с одинаковым пунктом назначения (dest), 
+которые прибывают в один и тот же день с интервалом не более 5 минут */;
+
+
+-- UNION // UNION ALL - накладывают поля друг на друга
+/* UNION - Если записи идентичны, UNION вернёт только одну запись
+   UNION ALL - Вернёт абсолютно  все */
+SELECT
+    CASE
+        WHEN UPPER(LEFT(name, 1)) IN ('A', 'E', 'I', 'O', 'U')
+        THEN 'airlines_vowel'
+        ELSE 'airlines_consonant'   
+    END AS entity_type, name
+FROM airlines
+UNION ALL
+
+SELECT 'airports', name -- для последующий SELECT в своей колонке не нужно указывать название
+FROM airports
+UNION ALL
+
+SELECT 'manufacturer', manufacturer
+FROM planes
+WHERE manufacturer IS NOT NULL; -- Исключаем пустые значения
+
+
+--INTERSECT
+
+--№1----------------------------
+-- Авиакомпании, которые летают из всех трех аэропортов NYC
+SELECT carrier FROM flights WHERE origin = 'JFK'
+INTERSECT
+SELECT carrier FROM flights WHERE origin = 'LGA'
+INTERSECT  
+SELECT carrier FROM flights WHERE origin = 'EWR';
+--------------------------
+
+--№2----------------------------
+-- Кто как часто летает из NYC
+SELECT carrier, COUNT(*) as flights_count
+FROM flights 
+WHERE carrier IN ('AA', 'B6', 'DL', 'UA', 'US', 'MQ', 'EV', '9E') -- результат предыдущего запроса
+GROUP BY carrier
+ORDER BY flights_count DESC;
+--------------------------
+
+--№3----------------------------
+-- Куда летали и American Airlines, и Delta
+SELECT dest 
+FROM flights
+WHERE carrier = 'AA'
+INTERSECT
+SELECT dest 
+FROM flights 
+WHERE carrier = 'DL';
+--------------------------
+
+
+-- EXCEPT
+-- Из каких аэропортов вылетают, в которые не прилетают?
+SELECT origin
+FROM flights
+EXCEPT
+SELECT dest
+FROM flights;
+
+-- В какие аэропорты прилетают, но из которых НЕ вылетают (из NYC)?
+SELECT dest
+FROM flights
+EXCEPT
+SELECT origin
+FROM flights;
+
+
+-- Самолеты, которые есть в регистре, но никогда не летали
+SELECT tailnum 
+FROM planes
+EXCEPT
+SELECT tailnum 
+FROM flights 
+WHERE tailnum IS NOT NULL; -- 998 самолётов ни разу не летали
